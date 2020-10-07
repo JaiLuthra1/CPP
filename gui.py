20b82217a591af9cd279ff265ef4f832dcc676e5
Jai Luthra <luthrajaiji@gmail.com>
2020-10-07 18:13:51 +0530

    GUI code formatted

--- gui.py
+++ gui.py
@@ -1,25 +1,30 @@
+# Interactive gui to get argv
+
 import sys
+
 if sys.version_info[0] == 3:
     # for Python3
-    from tkinter import *   ## notice lowercase 't' in tkinter here
+    from tkinter import *  ## notice lowercase 't' in tkinter here
 else:
     # for Python2
-    from Tkinter import *   ## notice capitalized T in Tkinter
+    from Tkinter import *  ## notice capitalized T in Tkinter
 import os
 
-root=Tk()
+root = Tk()
 
-root.title("Cpcp APP")
+root.title("CPP")
+
 
 def fun():
-    os.system("python3 cpp.py "+coi.get())
+    os.system("python3 cpp.py " + coi.get())
 
-e1=Label(root,text="Enter the Cont ID")
+
+e1 = Label(root, text="Enter the Cont ID")
 e1.pack()
 
-coi=Entry(root)
+coi = Entry(root)
 coi.pack()
 
-but=Button(root,text="Fetch it",command=fun)
+but = Button(root, text="Fetch it", command=fun)
 but.pack()
 root.mainloop()
